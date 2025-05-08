8d0a962d7a60710b52ece328d43a65f05cbcfb35
Nomura　Tetsuhiro <42858765+nomura-t@users.noreply.github.com>
2025-05-08 20:11:24 +0900

    localizeファイル追加

---
+++ RecallMate/String+Localization.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    func localized(comment: String) -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+    
+    func localizedFormat(_ arguments: CVarArg...) -> String {
+        return String(format: self.localized, arguments)
+    }
+    
+    // 数値付きのパラメータ化された文字列に便利
+    func localizedWithInt(_ value: Int) -> String {
+        return String(format: self.localized, value)
+    }
+    
+    // 日付付きのパラメータ化された文字列に便利
+    func localizedWithDate(_ date: Date, style: DateFormatter.Style = .medium) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = style
+        return String(format: self.localized, formatter.string(from: date))
+    }
+}
